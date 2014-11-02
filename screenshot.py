9b0969cdfe2eed69f40c75ca9ba5dcf06b2575fc
Jonas Konrad <me@yawk.at>
2014-11-02 15:11:22 +0100

    Screenshot from clipboard support

--- screenshot.py
+++ screenshot.py
@@ -30,92 +30,125 @@ def upload(image):
     pyperclip.copy(url)
     os.system("notify-send --expire-time=2000 \"Screenshot uploaded\" \"" + url + "\"")
 
-### take screenshot
+def take_screen():
+    ### take screenshot
 
-img = pyscreenshot.grab()
-img_dark = img.point(lambda c: c * 0.5)
-bbox = img.getbbox()
+    img = pyscreenshot.grab()
+    img_dark = img.point(lambda c: c * 0.5)
+    bbox = img.getbbox()
 
-### display
+    ### display
 
-frame = tk.Frame(width=bbox[2], height=bbox[3])
-# xlib hacks to float above WM
-frame.master.attributes("-type", "dock")
-frame.master.attributes("-topmost", "true")
+    frame = tk.Frame(width=bbox[2], height=bbox[3])
+    # xlib hacks to float above WM
+    frame.master.attributes("-type", "dock")
+    frame.master.attributes("-topmost", "true")
 
-# convert to PhotoImage for tk
-img_tk = ImageTk.PhotoImage(img)
+    # convert to PhotoImage for tk
+    img_tk = ImageTk.PhotoImage(img)
 
-canvas = tk.Canvas(frame, bg="#002b36", width=bbox[2], height=bbox[3])
-canvas.pack()
-canvas.place(x=-1, y=-1)
+    canvas = tk.Canvas(frame, bg="#002b36", width=bbox[2], height=bbox[3])
+    canvas.pack()
+    canvas.place(x=-1, y=-1)
 
-canvas.create_image((0, 0), image=img_tk, anchor=tk.NW)
+    canvas.create_image((0, 0), image=img_tk, anchor=tk.NW)
 
-area = 0, 0, 0, 0
-items = []
-
-def redraw():
-    global image_arr, items
-    for item in items:
-        canvas.delete(item)
+    area = 0, 0, 0, 0
     items = []
-    if area[2] is not 0 or area[3] is not 0:
-        top_left = area[0:2]
-        bottom_right = (area[0] + area[2], area[1] + area[3])
-        top_left, bottom_right = (
-            (min(top_left[0], bottom_right[0]), min(top_left[1], bottom_right[1])), 
-            (max(top_left[0], bottom_right[0]), max(top_left[1], bottom_right[1]))
-        )
-        slice_boxes = (
-            (0, 0, top_left[0], bbox[3]),
-            (top_left[0], 0, bottom_right[0], top_left[1]),
-            (bottom_right[0], 0, bbox[2], bbox[3]),
-            (top_left[0], bottom_right[1], bottom_right[0], bbox[3]),
-        )
+
+    def redraw():
+        global image_arr, items
+        for item in items:
+            canvas.delete(item)
+        items = []
+        if area[2] is not 0 or area[3] is not 0:
+            top_left = area[0:2]
+            bottom_right = (area[0] + area[2], area[1] + area[3])
+            top_left, bottom_right = (
+                (min(top_left[0], bottom_right[0]), min(top_left[1], bottom_right[1])), 
+                (max(top_left[0], bottom_right[0]), max(top_left[1], bottom_right[1]))
+            )
+            slice_boxes = (
+                (0, 0, top_left[0], bbox[3]),
+                (top_left[0], 0, bottom_right[0], top_left[1]),
+                (bottom_right[0], 0, bbox[2], bbox[3]),
+                (top_left[0], bottom_right[1], bottom_right[0], bbox[3]),
+            )
+        else:
+            # just the full screen
+            slice_boxes = (bbox,)
+
+        image_arr = []
+        for slice_box in slice_boxes:
+            slice_image = ImageTk.PhotoImage(img_dark.crop(slice_box))
+            image_arr.append(slice_image)
+            item = canvas.create_image(slice_box[:2], image=slice_image, anchor=tk.NW)
+            items.append(item)
+
+    redraw()
+
+    selecting = False
+
+    def down(evt):
+        global selecting, area
+        selecting = True
+        area = (evt.x, evt.y, 0, 0)
+    def move(evt):
+        global selecting, area
+        if selecting:
+            width = evt.x - area[0]
+            height = evt.y - area[1]
+            area = (area[0], area[1], width, height)
+            redraw()
+    def up(evt):
+        print "Shot", area
+        frame.master.destroy()
+        selected = img.crop((area[0], area[1], area[0] + area[2], area[1] + area[3]))
+        upload(selected)
+
+    frame.bind("<Escape>", sys.exit)
+    canvas.bind("<Button-1>", down)
+    canvas.bind("<B1-Motion>", move)
+    canvas.bind("<ButtonRelease-1>", up)
+
+    frame.pack()
+
+    # force focus on the frame
+    frame.after(0, lambda: frame.master.focus_force())
+    frame.focus_set()
+
+    frame.mainloop()
+
+    print "Done"
+
+def take_clipboard():
+    import gtk
+    clipboard = gtk.Clipboard()
+
+    text = clipboard.wait_for_text()
+    if text is not None:
+        print("Text: %s" % text)
+        try:
+            img = Image.open(text)
+            upload(img)
+        except:
+            print("Failed to use file source")
+
+    pixbuf = clipboard.wait_for_image()
+    if pixbuf is not None:
+        pixels = pixbuf.get_pixels()
+        if pixbuf.get_has_alpha():
+            colorspace = "RGBA"
+        else:
+            colorspace = "RGB"
+        img = Image.fromstring(colorspace, (pixbuf.get_width(), pixbuf.get_height()), pixels)
+        upload(img)
+        return
     else:
-        # just the full screen
-        slice_boxes = (bbox,)
+        print("Not an image")
 
-    image_arr = []
-    for slice_box in slice_boxes:
-        slice_image = ImageTk.PhotoImage(img_dark.crop(slice_box))
-        image_arr.append(slice_image)
-        item = canvas.create_image(slice_box[:2], image=slice_image, anchor=tk.NW)
-        items.append(item)
-
-redraw()
-
-selecting = False
-
-def down(evt):
-    global selecting, area
-    selecting = True
-    area = (evt.x, evt.y, 0, 0)
-def move(evt):
-    global selecting, area
-    if selecting:
-        width = evt.x - area[0]
-        height = evt.y - area[1]
-        area = (area[0], area[1], width, height)
-        redraw()
-def up(evt):
-    print "Shot", area
-    frame.master.destroy()
-    selected = img.crop((area[0], area[1], area[0] + area[2], area[1] + area[3]))
-    upload(selected)
-
-frame.bind("<Escape>", sys.exit)
-canvas.bind("<Button-1>", down)
-canvas.bind("<B1-Motion>", move)
-canvas.bind("<ButtonRelease-1>", up)
-
-frame.pack()
-
-# force focus on the frame
-frame.after(0, lambda: frame.master.focus_force())
-frame.focus_set()
-
-frame.mainloop()
-
-print "Done"
+if __name__ == '__main__':
+    if len(sys.argv) <= 1 or sys.argv[1] == "screen":
+        take_screen()
+    elif sys.argv[1] == "clipboard":
+        take_clipboard()
